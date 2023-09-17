40d1cc7b04c87829c9cc45c654565a4b347eeb0e
erickt2000 <114968994+erickt2000@users.noreply.github.com>
2023-09-17 19:55:12 -0400

    Create outcomes for game

--- data.js
+++ data.js
@@ -1,11 +1,24 @@
 const options = ["Rock", "Paper", "Scissors"];
 
 function getComputerChoice() {
-    return  options[Math.floor(Math.random() * options.length)];
+    const randomChoice = options[Math.floor(Math.random() * options.length)];
+    return randomChoice;
 }
 
 console.log(getComputerChoice()); // Working as intended
 
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === "Rock" && computerSelection === "Scissors" 
+    || playerSelection === "Paper" && computerSelection === "Rock"
+    || playerSelection === "Scissors" && computerSelection === "Paper") {
+        return "You Win!"
+    } else if (playerSelection === "Rock" && computerSelection === "Paper"
+    || playerSelection === "Paper" && computerSelection === "Scissors"
+    || playerSelection === "Scissors" && computerSelection === "Rock") {
+        return `You lost! ${computerSelection} beats ${playerSelection}`
+    } else {
+        return "It's a tie!"
+    }
+}
 
-
-
+getComputerChoice();
